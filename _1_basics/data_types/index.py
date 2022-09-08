e2c538cda99d170cba223b7f8059e8a6b24a96c4
Vikram Kumar <vikram.kuamr@squareboat.com>
2022-09-08 10:38:29 +0530

    links added

---
+++ _1_basics/data_types/index.py
@@ -0,0 +1,33 @@
+"""
+Variables can store data of different types, and different types can do different things. 
+
+Python has the following data types built-in by default, in these categories: 
+
+
+Text Type and Sequence Type: 
+str 
+
+Numeric Types: 
+int,float,complex 
+
+Sequence Types: 
+list,tuple,range 
+
+Mapping Type: 
+dict 
+
+Set Types: 
+set,frozenset 
+
+Boolean Type: 
+bool 
+
+Binary Types: 
+bytes,bytearray,memoryview 
+
+
+You can get the data type of any object by using the type() function: 
+
+Type Conversion 
+You can convert from one type to another with the int(), float(), and complex() methods: 
+"""
