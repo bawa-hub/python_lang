d70b0027bbc514d89587c1f0ebf559714c7219a0
Vikram kumar <vikram.kumar@squareboat.cm>
2023-04-06 19:24:04 +0530

    login system added

---
+++ _7_django/projects/studybud3.0_search/base/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.db.models import Q
+
+from .models import Room, Topic
+from .forms import RoomForm
+
+
+rooms = [
+    {'id': 1, 'name': 'learn python'},
+    {'id': 2, 'name': 'learn django'},
+    {'id': 3, 'name': 'learn frontend'}
+]
+
+
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__contains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    room_count = rooms.count()
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    # return HttpResponse("Room")
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if (form.is_valid()):
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if (form.is_valid()):
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
