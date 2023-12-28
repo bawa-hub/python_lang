ee913d8943be8e9d96ea0ef3b96f14b96ff8c7d8
vikram kumar <vikram.kumar@visionias.in>
2023-12-28 12:31:25 +0530

    folder modified

---
+++ _1_basics/_2_variables_namespaces_and_scopes/_0_.py
@@ -0,0 +1,2 @@
+# https://www.geeksforgeeks.org/namespaces-and-scope-in-python/
+# https://realpython.com/python-namespaces-scope/#modify-variables-out-of-scope
---
+++ _1_basics/_3_data_types/_5_dictionary/_2_comprehension.py
@@ -0,0 +1,41 @@
+square_dict = dict()
+for num in range(1, 11):
+    square_dict[num] = num*num
+print(square_dict)
+
+# above code using comprehension
+# dictionary comprehension example
+square_dict = {num: num*num for num in range(1, 11)}
+print(square_dict)
+
+
+# syntax
+# dictionary = {key: value for vars in iterable}
+
+#item price in dollars
+old_price = {'milk': 1.02, 'coffee': 2.5, 'bread': 2.5}
+
+dollar_to_pound = 0.76
+new_price = {item: value*dollar_to_pound for (item, value) in old_price.items()}
+print(new_price)
+
+# Conditionals in Dictionary Comprehension
+original_dict = {'jack': 38, 'michael': 48, 'guido': 57, 'john': 33}
+even_dict = {k: v for (k, v) in original_dict.items() if v % 2 == 0}
+print(even_dict)
+
+
+original_dict = {'jack': 38, 'michael': 48, 'guido': 57, 'john': 33}
+new_dict = {k: v for (k, v) in original_dict.items() if v % 2 != 0 if v < 40}
+print(new_dict)
+
+original_dict = {'jack': 38, 'michael': 48, 'guido': 57, 'john': 33}
+new_dict_1 = {k: ('old' if v > 40 else 'young')
+    for (k, v) in original_dict.items()}
+print(new_dict_1)
+
+# Nested Dictionary Comprehension
+dictionary = {
+    k1: {k2: k1 * k2 for k2 in range(1, 6)} for k1 in range(2, 5)
+}
+print(dictionary)
