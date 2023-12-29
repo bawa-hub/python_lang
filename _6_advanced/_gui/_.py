e600cd4efc861c62c59b1ab5fdcc1216d3af27c8
Bawa Vikram <bawa@LAPTOP-7JOC07FH>
2023-12-29 18:23:19 +0530

    books added

---
+++ _6_advanced/_gui/_.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("420x420")
+window.title("TODO")
+
+# icon = PhotoImage(file='bird.jpg')
+# window.iconphoto(True, icon)
+
+window.config(background="black")
+
+window.mainloop()
